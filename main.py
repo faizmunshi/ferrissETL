3c697ed7c1f7b0a0d079fe4d5776378001442f33
Piste <psteckm@gmail.com>
2016-08-12 22:10:00 -0400

    I have an XML file

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+#!/usr/bin/python
 import urllib
+import xmltodict
 
-FEED_URL = 'https://overcast.fm/itunes863897795/the-tim-ferriss-show'
+FEED_URL = 'https://feeds.feedburner.com/thetimferrissshow'
+FEED_FILE = 'rss.xml'
+# AUDIO_BOOK_PATTERN = '<a href= "http://www.audible.com'
 
 print "Fetching XML"
-urllib.urlretrieve (FEED_URL, "rss.xml")
+urllib.urlretrieve (FEED_URL, FEED_FILE)
 
+with open(FEED_FILE) as fd:
+    doc = xmltodict.parse(fd.read())
+
+print doc
