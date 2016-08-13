bb809281175e8af28b836f73f47f7ef398faa137
Piste <psteckm@gmail.com>
2016-08-13 00:37:14 -0400

    so close, can't build list of links

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python
 import urllib
 import xmltodict
+import unicodecsv
+from bs4 import BeautifulSoup
+
+import json
 
 FEED_URL = 'https://feeds.feedburner.com/thetimferrissshow'
 FEED_FILE = 'rss.xml'
+CSV_FILE = 'extract.csv'
 # AUDIO_BOOK_PATTERN = '<a href= "http://www.audible.com'
+
+findings = {}
 
 print "Fetching XML"
 urllib.urlretrieve (FEED_URL, FEED_FILE)
@@ -12,4 +19,36 @@ urllib.urlretrieve (FEED_URL, FEED_FILE)
 with open(FEED_FILE) as fd:
     doc = xmltodict.parse(fd.read())
 
-print doc
+for item in doc['rss']['channel']['item']:
+	findings[item['title']] = item['description']
+	soup = BeautifulSoup(item['description'], "html.parser")
+	#print(soup.prettify())
+	findings[item['title']] = []
+	link_enum = 0
+	for link in soup.find_all('a'):
+		if link.get('href').find("amazon") > 1:
+			findings[item['title']][link_enum] = {}
+			findings[item['title']][link_enum]['link'] = link.get('href')
+			findings[item['title']][link_enum]['type'] = 'Book?'
+			link_enum += 1
+			#print link.get('href')
+		if link.get('href').find('audible') > 1:
+			print link
+			#print link.get['href'
+			findings[item['title']][link_enum] = {}
+			findings[item['title']][link_enum]['link'] = link.get('href')
+			findings[item['title']][link_enum]['type'] = 'Audiobook?'
+			link_enum += 1
+			#print link.get('href')
+
+with open(CSV_FILE, 'w') as csvfile:
+	fieldnames = ['Episode', 'Description']
+	writer = unicodecsv.DictWriter(csvfile, fieldnames=fieldnames)
+
+	writer.writeheader()
+	for ep in findings:
+		#print ep
+		writer.writerow({'Episode': ep})
+
+#if removing the below, remove import
+# print json.dumps(findings)
